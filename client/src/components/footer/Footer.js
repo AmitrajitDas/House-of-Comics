34712f626f12ac10ff838f2be1a618ed756aa90d
Amitrajit Das <amitrajitdas31@gmail.com>
2021-06-05 20:41:06 +0530

    structuring headers and footers

---
+++ client/src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useStyles } from './styles'
+
+const Footer = () => {
+    
+    const classes = useStyles()
+    return (
+        <div>
+            Footer
+        </div>
+    )
+}
+
+export default Footer
---
+++ client/src/components/header/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useStyles } from './styles'
+
+const Navbar = () => {
+    
+    const classes = useStyles()
+    return (
+        <div>
+            Navbar
+        </div>
+    )
+}
+
+export default Navbar

3f997565096fa13c43a9429e8afd48b71cdf7da5
Amitrajit Das <amitrajitdas31@gmail.com>
2021-06-05 20:40:28 +0530

    structured and refactored code

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,38 @@
+import { muiThemeProvider, createMuiTheme, responsiveFontSizes, Container} from '@material-ui/core'
 
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
-function App() {
+import Navbar from './components/header/Navbar'
+import Footer from './components/footer/Footer'
+
+const App = () => {
+
+   const theme = responsiveFontSizes(
+    createMuiTheme({
+      typography: {
+        fontFamily: '"Segoe UI"',
+      },
+      palette: {
+        primary: { main: '#e4efe7' },
+        secondary: { main: '#1eae98' },
+      },
+    })
+  );
+
   return (
-    <div className="App">Smoothie App</div>
+    <>
+    <muiThemeProvider theme={theme}>
+      <Router>
+        <Navbar />
+        <Container>
+          <Switch>
+            <h1>Comixology</h1>
+          </Switch>
+        </Container>
+        <Footer />
+      </Router>
+    </muiThemeProvider>   
+    </>
   );
 }
 

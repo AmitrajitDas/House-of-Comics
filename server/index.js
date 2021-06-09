37b908251f648176bef8c307109064211450396b
Amitrajit Das <amitrajitdas31@gmail.com>
2021-06-09 20:07:27 +0530

    added dummy routes temporarily to test backend

--- server/index.js
+++ server/index.js
@@ -1,12 +1,28 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-dotenv.config();
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+import products from './data/products.js'
+
+dotenv.config()
 
 const app = express();
 app.use(cors());
 
+
+app.get('/', (req, res) => {
+  res.send('API is running....')
+})
+
+app.get('/api/products', (req, res) => {
+  res.json(products)
+})
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find(item => item._id === req.params.id)
+  res.json(product)
+})
 
 
 //////////////////  Database Connection ////////////////////

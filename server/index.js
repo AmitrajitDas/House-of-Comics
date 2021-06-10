7e4b5916b42245e069c40dbdc3f63d74843cd6aa
Amitrajit Das <amitrajitdas31@gmail.com>
2021-06-10 21:41:27 +0530

    user, product and order mmodels are created

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import colors from 'colors'
 
 import products from './data/products.js'
 import connectDB from './config/db.js'
@@ -28,15 +29,12 @@ app.get('/api/products/:id', (req, res) => {
 })
 
 
-//////////////////  Database Connection ////////////////////
-
-const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT = process.env.PORT|| 5000
 
 app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
 )
 
---
+++ server/models/productModel.js
@@ -0,0 +1,77 @@
+import mongoose from 'mongoose'
+
+const reviewSchema = mongoose.Schema({
+    name:{ type: String, required: true},
+    rating:{ type: Number, required: true},
+    comment:{ type: String, required: true}
+}, {
+    timestamps: true
+})
+
+const productSchema = mongoose.Schema({
+    
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        red: 'User'
+    },
+
+    name: {
+        type: String,
+        required: true
+    },
+
+    image: {
+        type: String,
+        required: true
+    },
+
+    description: {
+        type: String,
+        required: true,
+
+    },
+
+    publisher: {
+        type: String,
+        required: true
+    },
+
+    category: {
+        type: String,
+        required: true
+    },
+
+    price: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+
+    countInStock: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+
+    reviews: [reviewSchema],
+
+    rating: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+
+    numReviews: {
+        type: Number,
+        required: true,
+        default: 0
+    }
+    
+},  {
+        timestamps: true
+    })
+
+const Product = mongoose.model('Product', productSchema)
+
+export default Product

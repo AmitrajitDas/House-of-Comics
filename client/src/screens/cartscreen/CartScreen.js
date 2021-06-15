6c88ade0b961d590d1520eb270e43cd4db5ef38c
Amitrajit Das <amitrajitdas31@gmail.com>
2021-06-15 19:17:43 +0530

    initial structure for cart screen

---
+++ client/src/screens/cartscreen/CartScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { useStyles } from './styles'
+
+const CartScreen = () => {
+
+    const classes = useStyles()
+    return (
+        <div>
+            Cart
+        </div>
+    )
+}
+
+export default CartScreen

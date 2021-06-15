0a9b03f7b807623eced1579013dc031f087b9f82
Amitrajit Das <amitrajitdas31@gmail.com>
2021-06-15 19:16:02 +0530

    add item reducer with id match checking conditions

---
+++ client/src/redux/reducers/cartReducer.js
@@ -0,0 +1,28 @@
+import { CART_ADD_ITEM } from '../constants/cartConstants'
+
+export const cartReducer = (state = { cartItems: [] }, action) => {
+
+    switch(action.type){
+
+        case CART_ADD_ITEM: 
+            const item = action.payload
+
+            const isItem = state.cartItems.find(x => x.productId === item.productId)
+
+            if(isItem){
+                return{
+                    ...state,
+                    cartItems: state.cartItems.map(x => x.productId === isItem.productId ? item : x)
+                }
+
+            } else {
+                return {
+                    ...state,
+                    cartItems: [...state.cartItems, item]
+                }
+            }
+
+        default:
+            return state
+    }
+}

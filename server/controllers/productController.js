8d3a61bcef559f54d5c2f0317776604dbd636a1b
Amitrajit Das <amitrajitdas31@gmail.com>
2021-06-19 19:24:55 +0530

    added route controllers

---
+++ server/controllers/productController.js
@@ -0,0 +1,33 @@
+import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js'
+
+// @desc Fetch all products
+// @route GET /api/products
+// @access Public
+
+export const getProducts = asyncHandler( async(req, res) => {
+
+    const products = await Product.find({})
+    res.json(products)
+})
+
+// @desc Fetch a single product
+// @route GET /api/products/:id
+// @access Public
+
+export const getProductDetails = asyncHandler( async(req, res) => {
+
+    const product = await Product.findById(req.params.id)
+  
+    if(product) {
+
+        res.json(product)
+
+    } else {
+
+        throw new Error('Product not found')
+        res.status(404)
+
+    }
+
+})
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,39 +1,10 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
-import Product from '../models/productModel.js'
-
+import { getProducts, getProductDetails } from '../controllers/productController.js'
 const router = express.Router()
 
-// @desc Fetch all products
-// @route GET /api/products
-// @access Public
 
-router.get('/', asyncHandler(async(req, res) => {
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductDetails)
 
-    const products = await Product.find({})
-
-    res.json(products)
-}))
-
-// @desc Fetch single product
-// @route GET /api/products/:id
-// @access Public
-
-router.get('/:id', asyncHandler(async(req, res) => {
-
-  const product = await Product.findById(req.params.id)
-  
-  if(product){
-
-    res.json(product)
-
-  }else{
-
-    throw new Error('Product not found')
-    res.status(404)
-
-  }
-  
-}))
 
 export default router

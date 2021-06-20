40767f22982393c74045950efc359ebc568ad7b6
Amitrajit Das <amitrajitdas31@gmail.com>
2021-06-20 21:50:00 +0530

    user registration controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,7 +3,7 @@ import User from '../models/userModel.js'
 import generateAuthToken from '../utils/token.js'
 
 // @desc Authenticate users and get tokens
-// @route POST /api/user/login
+// @route POST /api/auth/login
 // @access Public
 
 export const userLogin = asyncHandler( async(req, res) => {
@@ -25,8 +25,43 @@ export const userLogin = asyncHandler( async(req, res) => {
         }
 })
 
+// @desc Registering by creating a new user
+// @route POST /api/auth/signup
+// @access Public
+
+export const userRegister = asyncHandler( async(req, res) => {
+
+    const { name, email, password } = req.body
+
+    const isUser = await User.findOne({ email })
+
+    if (isUser) {
+        res.status(400)
+        throw new Error('User exists already')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if(user){
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            password: user.password,
+            isAdmin: user.isAdmin,
+            token: generateAuthToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+})
+
 // @desc Get user profile
-// @route POST /api/user/profile
+// @route POST /api/auth/profile
 // @access Private
 
 export const getUserProfile = asyncHandler( async(req, res) => {

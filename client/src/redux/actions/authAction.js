0d093dec203a3f34b001aa937255157f5c37604e
Amitrajit Das <amitrajitdas31@gmail.com>
2021-06-21 15:00:33 +0530

    user login action creator

---
+++ client/src/redux/actions/authAction.js
@@ -0,0 +1,31 @@
+import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAILURE, USER_LOGOUT } from '../constants/authConstants'
+import { userLogin } from '../../api/users'
+
+export const userLoginAction = (email, password) => async(dispatch) => {
+
+    try {
+        
+        dispatch({ 
+            type: USER_LOGIN_REQUEST,
+        })
+
+        const { data } = await userLogin(email, password)
+
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data
+        })
+
+        localStorage.setItem('userData', JSON.stringify(data))
+
+    } catch (error) {
+        
+        dispatch({ 
+            type: USER_LOGIN_FAILURE,
+            payload: error.response && error.response.data.message 
+            ? error.response.data.message 
+            : error.message
+        })
+
+    }
+}

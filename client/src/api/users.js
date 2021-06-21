9d5af11c5bdc4d2f482047e8cd056a6253968c31
Amitrajit Das <amitrajitdas31@gmail.com>
2021-06-21 14:59:14 +0530

    userlogin api

---
+++ client/src/api/users.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const API = axios.create({ baseURL: `http://localhost:5000/api/auth` })
+
+const config = {
+
+    headers: {
+        "Content-Type": "application/json",
+    },
+}
+
+export const userLogin = (email, password) => API.post('/login', {email, password}, config)

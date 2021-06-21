9ee290015045227e77ab40389acd48ba8055cf09
Amitrajit Das <amitrajitdas31@gmail.com>
2021-06-21 15:11:54 +0530

    api bug solved

--- client/src/api/products.js
+++ client/src/api/products.js
@@ -4,4 +4,4 @@ const API = axios.create({ baseURL: `http://localhost:5000/api/products` })
 
 export const getProducts = () => API.get()
 
-export const getProductDetails = (id) => axios.get(`/${id}`)
+export const getProductDetails = (id) => API.get(`/${id}`)

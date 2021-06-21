4fba42b8af9456bd8c002ba285e0485a22e0a939
Amitrajit Das <amitrajitdas31@gmail.com>
2021-06-21 15:01:19 +0530

    setting userData initial state from local storage

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -5,20 +5,24 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer } from './reducers/productListReducer'
 import { productDetailsReducer } from './reducers/productDetailsReducer'
 import { cartReducer } from './reducers/cartReducer'
+import { userLoginReducer} from './reducers/authReducers'
 
 const middleware = [thunk]
 
 const reducers = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    userLogin: userLoginReducer,
 })
 
 
 const cartItemsFromStorage = localStorage.getItem('cartItem') ? JSON.parse(localStorage.getItem('cartItem')) : []
+const userDataFromStorage = localStorage.getItem('userData') ? JSON.parse(localStorage.getItem('userData')) : null
 
 const initialState = {
     cart: { cartItems: cartItemsFromStorage},
+    userLogin: { userData: userDataFromStorage },
 }
 
 

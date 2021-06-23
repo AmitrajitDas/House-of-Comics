31075f3c511de601a0a1c6e02b8e097f73ce6996
Amitrajit Das <amitrajitdas31@gmail.com>
2021-06-23 18:58:07 +0530

    update profile endpoint

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -84,3 +84,37 @@ export const getUserProfile = asyncHandler( async(req, res) => {
 
     }
 })
+
+// @desc Update user profile
+// @route PUT /api/auth/profile
+// @access Private
+
+export const updateUserProfile = asyncHandler( async(req, res) => {
+
+    const user = await User.findById(req.user._id)
+
+    if(user){
+
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        if(req.body.password){
+            user.password = req.body.password
+        }
+
+        const updatedUser = await user.save()
+
+         res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+            token: generateAuthToken(updatedUser._id)
+        })
+
+    } else {
+
+        res.status(404)
+        throw new Error('User not found!')
+
+    }
+})

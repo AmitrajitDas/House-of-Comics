52b2a625d090f0f6e5c46f7eb5a10533174bd53d
Amitrajit Das <amitrajitdas31@gmail.com>
2021-06-23 18:58:31 +0530

    protected put request with auth middleware

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import { userLogin, userRegister,getUserProfile } from '../controllers/userController.js'
+import { userLogin, userRegister,getUserProfile, updateUserProfile } from '../controllers/userController.js'
 import { routeProtection } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.route('/signup').post(userRegister)
 router.route('/login').post(userLogin)
-router.route('/profile').get(routeProtection, getUserProfile)
+router.route('/profile').get(routeProtection, getUserProfile).put(routeProtection, updateUserProfile)
 
 
 export default router

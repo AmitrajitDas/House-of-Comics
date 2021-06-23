9f0b6392fa2d42b99050484077fa3e86592edfa8
Amitrajit Das <amitrajitdas31@gmail.com>
2021-06-23 21:23:20 +0530

    user profile api

--- client/src/api/users.js
+++ client/src/api/users.js
@@ -5,10 +5,24 @@ const API = axios.create({ baseURL: `http://localhost:5000/api/auth` })
 const config = {
 
     headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
     },
 }
 
 export const userLogin = (email, password) => API.post('/login', {email, password}, config)
 
 export const userRegister = (name, email, password) => API.post('/signup', {name, email, password}, config)
+
+export const getUserProfile = (id, token) => {
+    
+    const config = {
+
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token} `, 
+            },
+        }
+    
+    
+    API.get(`/${id}`, config )
+}

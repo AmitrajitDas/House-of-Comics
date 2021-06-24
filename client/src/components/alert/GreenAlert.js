f82b4e35db85c094cfc9b0aa45733c7576955c4e
Amitrajit Das <amitrajitdas31@gmail.com>
2021-06-24 23:38:27 +0530

    updated alert component

--- client/src/components/alert/GreenAlert.js
+++ client/src/components/alert/GreenAlert.js
@@ -3,15 +3,15 @@ import Alert from '@material-ui/lab/Alert';
 import { useStyles } from './styles'
 
 
-const AlertBox = ({ alert }) => {
+const GreenAlertBox = ({ alert }) => {
 
     const classes = useStyles()
 
     return (
         <div className={classes.alert}>
-            <Alert severity='error'>{alert}</Alert>
+            <Alert severity='success'>{alert}</Alert>
         </div>
     )
 }
 
-export default AlertBox
+export default GreenAlertBox

ddc3359e997f35c239fa23612e7a4e1bb3c34c1c
Amitrajit Das <amitrajitdas31@gmail.com>
2021-07-06 13:03:31 +0530

    order reducers

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import rootReducers from './reducers/indexReducer'
+import rootReducers from './reducers/indexReducers'
 
 const middleware = [thunk]
 

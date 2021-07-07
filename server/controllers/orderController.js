8e1c7f93d629d8d3acd0c7e30cd3187159252d8e
Amitrajit Das <amitrajitdas31@gmail.com>
2021-07-07 19:03:54 +0530

    order paid controller

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -57,3 +57,32 @@ export const getOrderDetails = asyncHandler( async(req, res) => {
         throw new Error('Order not found')
     }
 })
+
+
+
+// @desc Update order to paid
+// @route GET /api/orders/:id/payment
+// @access Private
+
+export const updateOrderToPaid = asyncHandler( async(req, res) => {
+
+    const order = await Order.findById(req.params.id)
+
+    if(order){
+        order.isPaid = true
+        order.paidAt = Date.now()
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.payer.email_address,
+        }
+
+        const updatedOrder = await order.save()
+        res.json(updatedOrder)
+        
+    } else {
+        res.status(404)
+        throw new Error('Order not found')
+    }
+})

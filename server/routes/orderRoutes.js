3fbc2e4d0a5f5101a0cf489f0e074e8973f5cd45
Amitrajit Das <amitrajitdas31@gmail.com>
2021-07-07 12:02:02 +0530

    fetch order details route controllers

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { addOrderItems } from '../controllers/orderController.js'
+import { addOrderItems, getOrderDetails } from '../controllers/orderController.js'
 import { routeProtection } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.route('/').post(routeProtection, addOrderItems)
+router.route('/:id').get(routeProtection, getOrderDetails)
 
 export default router

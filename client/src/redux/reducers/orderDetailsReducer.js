0b8f1079e6a7d1b31007bdbc8da5eb5cdfc8b82d
Amitrajit Das <amitrajitdas31@gmail.com>
2021-07-07 12:42:43 +0530

    order details reducer

---
+++ client/src/redux/reducers/orderDetailsReducer.js
@@ -0,0 +1,21 @@
+import {    ORDER_DETAILS_REQUEST, 
+            ORDER_DETAILS_SUCCESS, 
+            ORDER_DETAILS_FAILURE 
+        } from '../constants/orderConstants'
+        
+
+export const orderDetailsReducer = ( state = { orderItems: [], shippingAddress: {} }, action) => {
+    switch(action.type){
+        case ORDER_DETAILS_REQUEST:
+            return {...state, loading: true }
+
+        case ORDER_DETAILS_SUCCESS:
+            return { loading: false, order: action.payload }
+
+        case ORDER_DETAILS_FAILURE:
+            return { loading: false, error: action.payload }
+            
+        default:
+            return state
+    }
+}

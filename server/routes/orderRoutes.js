aa8135bce4eb1fbed77cad45d041422375c869c4
Amitrajit Das <amitrajitdas31@gmail.com>
2021-07-07 19:04:11 +0530

    order paid endpoint created

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { addOrderItems, getOrderDetails } from '../controllers/orderController.js'
+import { addOrderItems, getOrderDetails, updateOrderToPaid } from '../controllers/orderController.js'
 import { routeProtection } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.route('/').post(routeProtection, addOrderItems)
 router.route('/:id').get(routeProtection, getOrderDetails)
+router.route('/:id/payment').put(routeProtection, updateOrderToPaid)
 
 export default router

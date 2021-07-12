139f334028bb4227fad992ad4a328441ae9577fb
Amitrajit Das <amitrajitdas31@gmail.com>
2021-07-12 23:42:49 +0530

    fixed minor bugs in paypal route

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,10 @@
-import { MuiThemeProvider, createMuiTheme, responsiveFontSizes, Container, Paper } from '@material-ui/core'
+import {
+  MuiThemeProvider,
+  createMuiTheme,
+  responsiveFontSizes,
+  Container,
+  Paper,
+} from '@material-ui/core'
 
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
@@ -18,47 +24,46 @@ import PlaceOrderScreen from './screens/placeorderscreen/PlaceOrderScreen'
 import OrderScreen from './screens/orderscreen/OrderScreen'
 
 const App = () => {
-
-   const theme = responsiveFontSizes(
+  const theme = responsiveFontSizes(
     createMuiTheme({
       typography: {
         fontFamily: '"Alegreya Sans"',
       },
       palette: {
         // type: 'dark',
-        primary: { main: '#8fd9a8', secondary: '#7eca9c'}, 
+        primary: { main: '#8fd9a8', secondary: '#7eca9c' },
         secondary: { main: '#ffffff' },
-        alternate: { main: '#161616', secondary: '#393e46'},
+        alternate: { main: '#161616', secondary: '#393e46' },
       },
     })
-  );
+  )
 
   return (
     <>
-    <MuiThemeProvider theme={theme}>
-    <Paper>
-      <Router>
-        <Navbar />
-        <Container>
-          <Switch>
-            <Route path='/' exact component={HomeScreen} />
-            <Route path='/product/:id' component={ProductScreen} />
-            <Route path='/cart/:id?' component={CartScreen} />
-            <Route path='/login' component={LoginScreen} />
-            <Route path='/signup' component={SignupScreen} />
-            <Route path='/profile' component={ProfileScreen} />
-            <Route path='/shipping' component={ShippingScreen} />
-            <Route path='/payment' component={PaymentScreen} />
-            <Route path='/placeorder' component={PlaceOrderScreen} />
-            <Route path='/order/:id' component={OrderScreen} />
-          </Switch>
-        </Container>
-        <Footer />
-      </Router>
-    </Paper>
-    </MuiThemeProvider>   
+      <MuiThemeProvider theme={theme}>
+        <Paper>
+          <Router>
+            <Navbar />
+            <Container>
+              <Switch>
+                <Route path='/' exact component={HomeScreen} />
+                <Route path='/product/:id' component={ProductScreen} />
+                <Route path='/cart/:id?' component={CartScreen} />
+                <Route path='/login' component={LoginScreen} />
+                <Route path='/signup' component={SignupScreen} />
+                <Route path='/profile' component={ProfileScreen} />
+                <Route path='/shipping' component={ShippingScreen} />
+                <Route path='/payment' component={PaymentScreen} />
+                <Route path='/placeorder' component={PlaceOrderScreen} />
+                <Route path='/order/:id' component={OrderScreen} />
+              </Switch>
+            </Container>
+            <Footer />
+          </Router>
+        </Paper>
+      </MuiThemeProvider>
     </>
-  );
+  )
 }
 
 export default App

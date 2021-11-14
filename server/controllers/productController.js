5abc7f404d2045654ee9cea8df1e4a8dcc4fa23c
AmitrajitDas <amitrajitdas31@gmail.com>
2021-11-15 01:06:58 +0530

    feat: createProduct and updateProduct endpoints

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -42,3 +42,56 @@ export const deleteProductById = asyncHandler(async (req, res) => {
     throw new Error('Product not found')
   }
 })
+
+// @desc    Create a product
+// @route   POST /api/products
+// @access  Private/Admin
+export const createProduct = asyncHandler(async (req, res) => {
+  try {
+    const product = new Product({
+      user: req.user._id,
+      name: 'Sample name',
+      image: '/images/sample.jpg',
+      description: 'Sample description',
+      publisher: 'Sample publisher',
+      category: 'Sample category',
+      price: 0,
+      countInStock: 0,
+      numReviews: 0,
+    })
+
+    const createdProduct = await product.save()
+    res.status(201).json(createdProduct)
+  } catch (error) {
+    res.status(404)
+    throw new Error('Error creating product')
+  }
+})
+
+// @desc    Update a product
+// @route   PUT /api/products/:id
+// @access  Private/Admin
+export const updateProduct = asyncHandler(async (req, res) => {
+  try {
+    const { name, price, description, image, brand, category, countInStock } =
+      req.body
+
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+      product.name = name
+      product.price = price
+      product.description = description
+      product.image = image
+      product.brand = brand
+      product.category = category
+      product.countInStock = countInStock
+
+      const updatedProduct = await product.save()
+      res.json(updatedProduct)
+    }
+  } catch (error) {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+})
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -3,16 +3,23 @@ import {
   getProducts,
   getProductDetails,
   deleteProductById,
+  createProduct,
+  updateProduct,
 } from '../controllers/productController.js'
 import { routeProtection } from '../middleware/authMiddleware.js'
 import { adminOnly } from '../middleware/adminMiddleware.js'
 
 const router = express.Router()
 
-router.route('/').get(getProducts)
+router
+  .route('/')
+  .get(getProducts)
+  .post(routeProtection, adminOnly, createProduct)
+
 router
   .route('/:id')
   .get(getProductDetails)
   .delete(routeProtection, adminOnly, deleteProductById)
+  .put(routeProtection, adminOnly, updateProduct)
 
 export default router

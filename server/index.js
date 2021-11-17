ea66bf3b21e2b7a4068acf7c38dde6c7ff5dfbd6
AmitrajitDas <amitrajitdas31@gmail.com>
2021-11-17 19:33:03 +0530

    feat: upload endpoint using multer

--- server/index.js
+++ server/index.js
@@ -3,11 +3,12 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import colors from 'colors'
-
+import path from 'path'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
@@ -19,6 +20,9 @@ app.use(cors())
 
 app.use(express.json())
 
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
 app.get('/', (req, res) => {
   res.send('API is running....')
 })
@@ -26,6 +30,7 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/auth', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)

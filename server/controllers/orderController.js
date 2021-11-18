002b3d18a76fc7507d0dc2ea50003ff44bc30442
AmitrajitDas <amitrajitdas31@gmail.com>
2021-11-18 13:25:51 +0530

    feat: get oders endpoint

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -89,3 +89,11 @@ export const getUserOrders = asyncHandler(async (req, res) => {
   const orders = await Order.find({ user: req.user.id })
   res.json(orders)
 })
+
+// @desc    Get all orders
+// @route   GET /api/orders
+// @access  Private/Admin
+export const getOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({}).populate('user', 'id name')
+  res.json(orders)
+})
--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -4,12 +4,17 @@ import {
   getOrderDetails,
   updateOrderToPaid,
   getUserOrders,
+  getOrders,
 } from '../controllers/orderController.js'
 import { routeProtection } from '../middleware/authMiddleware.js'
+import { adminOnly } from '../middleware/adminMiddleware.js'
 
 const router = express.Router()
 
-router.route('/').post(routeProtection, addOrderItems)
+router
+  .route('/')
+  .post(routeProtection, addOrderItems)
+  .get(routeProtection, adminOnly, getOrders)
 router.route('/myorders').get(routeProtection, getUserOrders)
 router.route('/:id').get(routeProtection, getOrderDetails)
 router.route('/:id/payment').put(routeProtection, updateOrderToPaid)

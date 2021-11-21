3fc8860483f09f1e7f6e7b460568bf1729872076
AmitrajitDas <amitrajitdas31@gmail.com>
2021-11-21 23:45:29 +0530

    feat(redux): product review create actions

--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -14,6 +14,9 @@ import {
   PRODUCT_UPDATE_REQUEST,
   PRODUCT_UPDATE_SUCCESS,
   PRODUCT_UPDATE_FAILURE,
+  PRODUCT_CREATE_REVIEW_REQUEST,
+  PRODUCT_CREATE_REVIEW_SUCCESS,
+  PRODUCT_CREATE_REVIEW_FAILURE,
 } from '../constants/productConstants'
 import axios from 'axios'
 
@@ -164,3 +167,41 @@ export const productUpdateAction = (product) => async (dispatch, getState) => {
     })
   }
 }
+
+export const productReviewCreateAction =
+  (productId, review) => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: PRODUCT_CREATE_REVIEW_REQUEST,
+      })
+
+      const {
+        userLogin: { userInfo },
+      } = getState()
+
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      }
+
+      await axios.post(
+        `${process.env.REACT_APP_DEV_API}/products/${productId}/reviews`,
+        review,
+        config
+      )
+
+      dispatch({
+        type: PRODUCT_CREATE_REVIEW_SUCCESS,
+      })
+    } catch (error) {
+      dispatch({
+        type: PRODUCT_UPDATE_FAILURE,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      })
+    }
+  }

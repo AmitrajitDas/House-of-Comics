0a109cd2498665f9126099847a20df7bdcd97126
AmitrajitDas <amitrajitdas31@gmail.com>
2022-04-08 17:26:01 +0530

    make it better

--- config/db.js
+++ config/db.js
@@ -2,14 +2,11 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      'mongodb+srv://adbrock31:2581@cluster0.hlokx.mongodb.net/HouseOfComics?retryWrites=true&w=majority',
-      {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-        useCreateIndex: true,
-      }
-    )
+    const conn = await mongoose.connect(process.env.CONNECTION_URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    })
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
   } catch (error) {
